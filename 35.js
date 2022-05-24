b380b5e4221811cc4c617fe432f79e3c9bdb5387
ionutz <ionutz_vig@yahoo.com>
2022-05-24 10:50:11 +0200

    mi primer comit

---
+++ 35.js
@@ -0,0 +1,34 @@
+var numero;
+
+var numero=0;
+
+for (numero = 1; numero <= 10; numero++) {
+ 
+	tabla=1*numero;
+console.log(tabla)
+}
+for (numero = 1; numero <= 10; numero++) {
+ 
+	tabla=2*numero;
+console.log(tabla)
+}
+for (numero = 1; numero <= 10; numero++) {
+ 
+	tabla=3*numero;
+console.log(tabla)
+}
+for (numero = 1; numero <= 10; numero++) {
+ 
+	tabla=4*numero;
+console.log(tabla)
+}
+
+ 
+for (numero = 1; numero <= 10; numero++) {
+ 
+	tabla=5*numero;
+console.log(tabla)
+}
+
+
+ 
---
+++ app_34.js
@@ -0,0 +1,18 @@
+
+
+let tabla = prompt("escoge la tabla de multiplicacion"); 
+console.log("La tabla del "  +  tabla  +  " es ");
+
+
+ for ( let index = 0; index <=10 ; index++) {
+   document.write(tabla + "*"+ index + "=" +index*tabla + "<br>")
+    console.log( tabla + "*"+ index + "=" +index*tabla)
+}
+
+
+
+
+ 
+   
+   
+
